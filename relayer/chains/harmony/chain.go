ae4db4fecdc53efb3464f595e71af6a83a7e82f2
mapdev33 <>
2023-06-28 17:36:37 +0800

    update the QueryClientState

--- relayer/chains/harmony/chain.go
+++ relayer/chains/harmony/chain.go
@@ -231,7 +231,7 @@ func (c *Chain) QueryClientState(height int64) (*clienttypes.QueryClientStateRes
 	if err != nil {
 		return nil, err
 	} else if !found {
-		return nil, fmt.Errorf("client not found: %v", c.pathEnd.ClientID)
+		return nil, fmt.Errorf("client not found: %v，%d", c.pathEnd.ClientID, height)
 	}
 	var clientState exported.ClientState
 	if err := c.Codec().UnmarshalInterface(s, &clientState); err != nil {
